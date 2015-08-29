c5d9bbd595d45d658891b8774f03cfb0297de64b
Sanat Tripathi <stripathi@zonoff.com>
2015-08-28 20:34:30 -0400

    Setting up Robolectric for unit testing

---
+++ Incubee_Android_Studio/app/src/test/java/incubee/android/IncubeeRobolectricTestRunner.java
@@ -0,0 +1,51 @@
+package incubee.android;
+
+import android.app.Activity;
+
+import org.junit.runners.model.InitializationError;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.manifest.AndroidManifest;
+import org.robolectric.res.Fs;
+
+public class IncubeeRobolectricTestRunner extends RobolectricTestRunner{
+    /**
+     * Creates a runner to run {@code testClass}. Looks in your working directory for your AndroidManifest.xml file
+     * and res directory by default. Use the {@link Config} annotation to configure.
+     *
+     * @param testClass the test class to be run
+     * @throws InitializationError if junit says so
+     */
+    public IncubeeRobolectricTestRunner(Class<?> testClass) throws InitializationError {
+        super(testClass);
+        String buildVariant = (BuildConfig.FLAVOR.isEmpty() ? "" : BuildConfig.FLAVOR + "/") + BuildConfig.BUILD_TYPE;
+        String intermediatesPath = BuildConfig.class.getResource("").toString().replace("file:", "");
+        intermediatesPath = intermediatesPath.substring(0, intermediatesPath.indexOf("/classes"));
+
+        System.setProperty("android.package", BuildConfig.APPLICATION_ID);
+        System.setProperty("android.manifest", intermediatesPath + "/manifests/full/" + buildVariant + "/AndroidManifest.xml");
+        System.setProperty("android.resources", intermediatesPath + "/res/merged/" + buildVariant);
+        System.setProperty("android.assets", intermediatesPath + "/assets/" + buildVariant);
+    }
+
+    @Override
+    protected AndroidManifest getAppManifest(Config config) {
+
+        AndroidManifest manifest = new AndroidManifest(Fs.fileFromPath(System.getProperty("android.manifest")),
+                Fs.fileFromPath(System.getProperty("android.resources")),
+                Fs.fileFromPath(System.getProperty("android.assets"))) {
+            @Override
+            public int getTargetSdkVersion() {
+                return 18;
+            }
+
+            @Override
+            public String getThemeRef(Class<? extends Activity> activityClass) {
+                return "@style/RoboAppTheme";
+            }
+        };
+
+        return manifest;
+    }
+
+}
---
+++ Incubee_Android_Studio/app/src/test/java/incubee/android/LoginActivityTest.java
@@ -0,0 +1,26 @@
+package incubee.android;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowHandler;
+import org.robolectric.shadows.ShadowToast;
+
+/**
+ * Test against LoginActivity.
+ */
+@RunWith(IncubeeRobolectricTestRunner.class)
+@Config(sdk = 21)
+public class LoginActivityTest {
+
+    @Test
+    public void testSignInButton() {
+        LoginActivity loginActivity = Robolectric.setupActivity(LoginActivity.class);
+        loginActivity.getWindow().findViewById(R.id.sign_in_button).performClick();
+        ShadowHandler.idleMainLooper();
+        Assert.assertEquals(ShadowToast.getTextOfLatestToast(), "Signing In...");
+    }
+}
