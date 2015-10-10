ae4a26cede19b47f808c2ebd40abd28637f04895
Samuh <samuhvarta@incub.ee>
2015-10-10 17:07:38 -0400

    Re-factored class hierarchy to keep Google Signin related code in one place

---
+++ Incubee_Android_Studio/app/src/main/java/incubee/android/activities/BaseActivity.java
@@ -0,0 +1,9 @@
+package incubee.android.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by samuh on 10/10/2015.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+}
